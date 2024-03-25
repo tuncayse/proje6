898d5067eafd5f36a8a8dac503f0f1727fc13272
Ayşe Tuç <143759353+tuncayse@users.noreply.github.com>
2024-03-25 23:46:24 +0300

    Update VaccineService.js

--- src/services/VaccineService.js
+++ src/services/VaccineService.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/api/v1/vaccine";
+const BASE_URL = "https://proje6backend.onrender.com/api/v1/vaccine";
 
 export const VaccineService = {
     getAllVaccines: async () => {
