6622bf74ebf2eef0e876bbd21bcc61e1199b74f5
Ayşe Tuç <143759353+tuncayse@users.noreply.github.com>
2024-03-25 23:44:57 +0300

    Update AnimalService.js

--- src/services/AnimalService.js
+++ src/services/AnimalService.js
@@ -1,4 +1,4 @@
-const ANIMAL_BASE_URL = "http://vett-backend.onrender.com/api/v1/animal";
+const ANIMAL_BASE_URL = "https://proje6backend.onrender.com/api/v1/animal";
 
 export const AnimalService = {
     getAllAnimals: async () => {
