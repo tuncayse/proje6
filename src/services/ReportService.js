5065afa9b4a9c819fc2a28ac9646489cc368e992
Ayşe Tuç <143759353+tuncayse@users.noreply.github.com>
2024-03-25 23:46:08 +0300

    Update ReportService.js

--- src/services/ReportService.js
+++ src/services/ReportService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3000/api/v1/report'; 
+const BASE_URL = 'https://proje6backend.onrender.com/api/v1/report'; 
 
 const getReports = async () => {
   try {
