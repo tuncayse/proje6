382528b4187c60dd763b667d7b393a80083cc356
Ayşe Tuç <143759353+tuncayse@users.noreply.github.com>
2024-03-26 00:31:12 +0300

    Update AvailableDateService.js

--- src/services/AvailableDateService.js
+++ src/services/AvailableDateService.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://localhost:3000/api/v1/available_date";
+const BASE_URL = "https://proje6backend.onrender.com/api/v1/available_date";
 
 export const AvailableDateService = {
     getAllDates: async () => {
