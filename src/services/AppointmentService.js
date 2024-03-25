67eece5bbb3af35b4c59586d9319a0c890efbecc
Ayşe Tuç <143759353+tuncayse@users.noreply.github.com>
2024-03-26 00:24:40 +0300

    Update AppointmentService.js

--- src/services/AppointmentService.js
+++ src/services/AppointmentService.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/api/v1/appointmentDate";
+const BASE_URL = "https://localhost:3000/api/v1/appointmentDate";
 
 export const AppointmentService = {
     getAllAppointments: async () => {
