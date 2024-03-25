fa4e1e22853d74fca09d04c07e09176500511312
Ayşe Tuç <143759353+tuncayse@users.noreply.github.com>
2024-03-26 00:24:49 +0300

    Update AvailableDateService.js

--- src/services/AvailableDateService.js
+++ src/services/AvailableDateService.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/api/v1/available_date";
+const BASE_URL = "https://localhost:3000/api/v1/available_date";
 
 export const AvailableDateService = {
     getAllDates: async () => {
