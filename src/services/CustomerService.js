59d27f381c31d5e52471913f13a383754d048a70
Ayşe Tuç <143759353+tuncayse@users.noreply.github.com>
2024-03-26 01:57:18 +0300

    Update CustomerService.js

--- src/services/CustomerService.js
+++ src/services/CustomerService.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/api/v1/customer";
+const BASE_URL = "https://proje6backendfinalyy.onrender.com/api/v1/customer";
 
 export const CustomerService = {
     getAllCustomers: async () => {
